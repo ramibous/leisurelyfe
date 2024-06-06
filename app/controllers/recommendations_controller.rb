aa4c15eeaabc7411f8e1d12ea4a33aa5f9e976d1
JUSTIN BAUTISTA <justinbautista0921@gmail.com>
2024-06-06 16:51:32 -0400

    Search Filter

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -1,6 +1,9 @@
 class RecommendationsController < ApplicationController
   def index
     @recommendations = Recommendation.all
+
+    @recommendations = @recommendations.where(kid_friendly: true) if params[:kid_friendly] == "1"
+    @recommendations = @recommendations.where(dog_friendly: true) if params[:dog_friendly] == "1"
   end
 
   def show
