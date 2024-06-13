649341bdb315a8136b6af7d817c624e016a516e3
rami boussarsar <ramibousarsar@gmail.com>
2024-06-13 12:37:22 -0400

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,73 +13,6 @@ puts "Done!"
 
 puts "Creating recommendations..."
 
-Recommendation.create!(
-  name: "Tommy Cafe",
-  description: "A cozy cafe in Old Montreal",
-  category: :restaurant,
-  price_range: "$$ - $$$",
-  price: 15,
-  address: "200 rue Notre-Dame O, Montreal, Quebec H2Y 1Z5 Canada",
-  longitude: -73.55742,
-  latitude: 45.50371,
-  kid_friendly: false,
-  dog_friendly: false,
-  outdoor: false,
-  image_url: "https://media-cdn.tripadvisor.com/media/photo-o/0e/d9/3c/1b/20170401-160920-largejpg.jpg",
-  start_time: '2024-06-13 11:30:00',
-  end_time: '2024-06-13 15:00:00'
-)
-
-Recommendation.create!(
-  name: "Resto Vego",
-  description: "None",
-  category: :restaurant,
-  price_range: "$$ - $$$",
-  price: 20,
-  address: "1720, rue Saint-Denis, Montreal, Quebec H2X 3K6 Canada",
-  longitude: -73.56461,
-  latitude: 45.515472,
-  kid_friendly: false,
-  dog_friendly: false,
-  outdoor: true,
-  image_url: "https://media-cdn.tripadvisor.com/media/photo-o/0b/6e/31/b5/img-20160526-145559-largejpg.jpg",
-  start_time: '2024-06-13 11:30:00',
-  end_time: '2024-06-13 15:00:00'
-)
-
-Recommendation.create!(
-  name: "Pizzedelic Boucherville",
-  description: "None",
-  category: :restaurant,
-  price_range: "$$ - $$$",
-  price: 18,
-  address: "1155 Place Nobel # A, Boucherville, Quebec J4B 7L3 Canada",
-  longitude: -73.44793,
-  latitude: 45.571327,
-  kid_friendly: true,
-  dog_friendly: false,
-  outdoor: false,
-  image_url: "https://media-cdn.tripadvisor.com/media/photo-o/0d/b2/c7/9c/photo0jpg.jpg",
-  start_time: '2024-06-13 11:30:00',
-  end_time: '2024-06-13 15:00:00'
-)
-
-Recommendation.create!(
-  name: "Trattoria Trestevere",
-  description: "None",
-  category: :restaurant,
-  price_range: "$$ - $$$",
-  price: 25,
-  address: "1237 rue Crescent, Montreal, Quebec H3G 2B1 Canada",
-  longitude: -73.574974,
-  latitude: 45.496826,
-  kid_friendly: false,
-  dog_friendly: true,
-  outdoor: false,
-  image_url: "https://media-cdn.tripadvisor.com/media/photo-o/0e/02/b2/4e/photo0jpg.jpg",
-  start_time: '2024-06-13 11:30:00',
-  end_time: '2024-06-13 15:00:00'
-)
 
 Recommendation.create!(
   name: "Saiko Bistrot Izakaya",
@@ -93,7 +26,7 @@ Recommendation.create!(
   kid_friendly: true,
   dog_friendly: false,
   outdoor: false,
-  image_url: "https://media-cdn.tripadvisor.com/media/photo-o/1b/0a/ff/c6/photo0jpg.jpg",
+  image_url: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/07/c9/53/cd/view-from-inside.jpg?w=1100&h=-1&s=1",
   start_time: '2024-06-13 11:30:00',
   end_time: '2024-06-13 15:00:00'
 )
@@ -103,118 +36,92 @@ Recommendation.create!(
   description: "Boustan is a Montreal-based restaurant known for its Middle-Eastern cuisine. Established in 1986, Boustan has been voted Best Middle-Eastern Restaurant in the Montreal Mirror in 2004, 2006, 2007, 2008, 2009, 2010, 2011 and 2012. Best Middle-Eastern and Late-Night Eats in the CultMTL Best of Montreal 2013, 2014, 2015 and 2016 editions.",
   category: :restaurant,
   price_range: "$",
-  price: 10,
+  price: 50,
   address: "2020A Crescent, Montreal, Quebec H3G 2B6 Canada",
   longitude: -73.577644,
   latitude: 45.498184,
-  kid_friendly: false,
-  dog_friendly: true,
+  kid_friendly: true,
+  dog_friendly: false,
   outdoor: false,
   image_url: "https://media-cdn.tripadvisor.com/media/photo-o/08/8f/0a/18/boustan.jpg",
-  start_time: '2024-06-13 11:30:00',
-  end_time: '2024-06-13 15:00:00'
+  start_time: '11:30:00',
+  end_time: '15:00:00'
 )
 
 Recommendation.create!(
-  name: "Pointe-a-Calliere, Montreal Archaeology and History Complex",
-  description: "Pointe-a-Calliere, the birthplace of Montreal and a National Historic Site, offers visitors the opportunity to explore history through an original underground circuit leading through archaeological excavations. At the start of their visit, an immersive multimedia show presents a high-speed view of the city's history, the remains come to life and the count down begins! The museum also offers year-round temporary local and international exhibitions. For Montreal's 375th anniversary in May 2017, Pointe-a-Calliere inaugurated a new pavilion highlighting Fort Ville-Marie.",
-  category: :attraction,
-  price_range: "$$",
-  price: 25,
-  address: "350 Place Royale, Montreal, Quebec H2Y 3Y5 Canada",
-  longitude: -73.55417,
-  latitude: 45.502644,
-  kid_friendly: false,
-  dog_friendly: false,
-  outdoor: true,
-  image_url: "https://media-cdn.tripadvisor.com/media/photo-o/07/d9/1b/8b/pointe-a-calliere-museum.jpg",
-  start_time: '2024-06-13 11:30:00',
-  end_time: '2024-06-13 15:00:00'
-)
-
-Recommendation.create!(
-  name: "Le Passe Compose",
-  description: "None",
-  category: :restaurant,
-  price_range: "$$ - $$$",
-  price: 30,
-  address: "1310 Boulevard de Maisonneuve East Àu Coin De La Visitation, Montreal, Quebec H2L 2A5 Canada",
-  longitude: -73.55661,
-  latitude: 45.520016,
+  name: "Montreal Botanical Garden",
+  description: "Recognized as one of the world’s finest, the Montréal Botanical Garden is a true living museum featuring plants from every part of the globe. This jewel in the city’s crown, founded in 1931, covers 75 hectares and features some 20 thematic gardens, a vast arboretum, the Frédéric Back Tree Pavilion and 3 exceptional cultural gardens: the Chinese Garden, Japanese Garden and First Nations Garden. Enjoy a colourful program of events, exhibitions and activities all year long.",
+  category: "park",
+  price_range: "$",
+  price: 10,
+  address: "200 rue Notre-Dame O, Montreal, Quebec H2Y 1Z5 Canada",
+  longitude: -73.55586320495809,
+  latitude:  45.561696508921194,
   kid_friendly: true,
-  dog_friendly: true,
-  outdoor: true,
-  image_url: "https://media-cdn.tripadvisor.com/media/photo-o/0f/d7/5d/54/la-terrasse.jpg",
-  start_time: '2024-06-13 11:30:00',
-  end_time: '2024-06-13 15:00:00'
-)
-Recommendation.create!(
-  name: "Restaurant Candide",
-  description: "Le restaurant Candide welcomes you to their dining room. They are open at night from Wednesday to Sunday. You can pay by MasterCard, Visa, gift certificates and Interac.",
-  category: :restaurant,
-  price_range: "$$ - $$$",
-  price: 110,
-  address: "551 Rue St. Martin, Montreal, Quebec H3J 2L6 Canada",
-  longitude: -73.56927,
-  latitude: 45.48901,
-  kid_friendly: false,
-  dog_friendly: true,
-  outdoor: true,
-  image_url: "https://media-cdn.tripadvisor.com/media/photo-o/0a/b8/dc/97/candide.jpg",
-  start_time: '2024-06-13 11:30:00',
-  end_time: '2024-06-13 15:00:00'
-)
-
-Recommendation.create!(
-  name: "Mano Cornuto",
-  description: "None",
-  category: :restaurant,
-  price_range: "$$ - $$$",
-  price: 30,
-  address: "988 Ottawa, Montreal, Quebec H3C 1S3 Canada",
-  longitude: -73.55825,
-  latitude: 45.49525,
-  kid_friendly: false,
   dog_friendly: false,
-  outdoor: false,
-  image_url: "https://media-cdn.tripadvisor.com/media/photo-m/1280/1b/1b/db/b1/photo0jpg.jpg",
-  start_time: '2024-06-13 11:30:00',
-  end_time: '2024-06-13 15:00:00'
-)
-
-Recommendation.create!(
-  name: "Notre Boeuf de Grace",
-  description: "Fresh beef ground daily on site",
-  category: :restaurant,
-  price_range: "$$ - $$$",
-  price: 30,
-  address: "5732 Rue Sherbrooke O, Montreal, Quebec H4A 1W8 Canada",
-  longitude: -73.61606,
-  latitude: 45.470432,
-  kid_friendly: false,
-  dog_friendly: true,
   outdoor: true,
-  image_url: "https://media-cdn.tripadvisor.com/media/photo-o/0f/f8/4e/39/l-orozco.jpg",
-  start_time: '2024-06-13 11:30:00',
-  end_time: '2024-06-13 15:00:00'
-)
+  image_url: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/2b/ae/2d/88/etang-du-jardin-japonais.jpg?w=1400&h=-1&s=1",
+  start_time: "09:00:00",
+  end_time: "17:00:00",
+  web_url: "https://www.espacepourlavie.ca/en/botanical-garden",
+  )
+  Recommendation.create!(
+    name: "Chateau Ramezay Historic Site and Museum of Montreal",
+    description: "Historic Site and Museum of Montreal. Visit Montreal’s portal to its past, the first building in Quebec to be classified an historic monument! Chosen as one of the 1001 Historic Sites You Must See Before You Die, the Château Ramezay invites you to relive, with its rich collections and multiple exhibitions, over 500 years of history, from pre-contact to the 20th century. In addition to the multimedia displays’ historical figures, costumed animators and guides will transport you through time. From the sumptuous Salle de Nantes, to the vaults where splendors and misery of New France come to life, and with the beautiful Governor’s Garden as a bonus – this is a not-to-be-missed landmark in the heart of Old Montreal!",
+    category: "park",
+    price_range: "$",
+    price: 10,
+    address: "200 rue Notre-Dame O, Montreal, Quebec H2Y 1Z5 Canada",
+    longitude: -73.55483899346075,
+    latitude: 45.51278140190878,
+    kid_friendly: true,
+    dog_friendly: false,
+    outdoor: true,
+    image_url: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/17/e0/7a/b2/le-chateau-ramesay-un.jpg?w=1400&h=-1&s=1",
+    start_time: "09:00:00",
+    end_time: "17:00:00",
+    web_url: "https://www.espacepourlavie.ca/en/botanical-garden",
+    )
 
-Recommendation.create!(
-  name: "Moghel Tandoori",
-  description: "None",
-  category: :restaurant,
-  price_range: "$$ - $$$",
-  price: 1000,
-  address: "538 Avenue Victoria, Saint-Lambert, Quebec J4P 2J5 Canada",
-  longitude: -73.51053,
-  latitude: 45.50129,
-  kid_friendly: false,
-  dog_friendly: false,
-  outdoor: false,
-  image_url: "https://media-cdn.tripadvisor.com/media/photo-s/07/94/5d/64/moghel-tandoori.jpg",
-  start_time: '2024-06-13 11:30:00',
-  end_time: '2024-06-13 15:00:00'
-)
+  Recommendation.create!(
+    name: " Bike Tour: Downtown, Old Montreal, Waterfront",
+    description: "Follow Montreal bike paths on a guided bike tour of Mont-Royal Park, Old Montreal, and the Saint Lawrence River, exploring local history and some of the city’s most important sites along the way. With a relaxed pace and plenty of breaks, this Montreal bike tour is a great introduction to the city's vibrant cycling culture, and it includes a bike, helmet, and water bottle.",
+    category: "biking",
+    price_range: "$",
+    price: 86,
+    address: "1455, Peel street 3rd Floor, Montreal, Quebec H3A 1T5 Canada",
+  longitude: -73.55292486749346,
+  latitude: 45.50774092165434,
+    kid_friendly: false,
+    dog_friendly: false,
+    outdoor: true,
+    image_url: "https://media-cdn.tripadvisor.com/media/attractions-splice-spp-720x480/09/34/5d/63.jpg",
+    start_time: "10:00:00",
+    end_time: "15:00:00",
+    web_url: "https://www.tripadvisor.ca/AttractionProductReview-g155032-d12027499-Montreal_Highlights_Bike_Tour_Downtown_Old_Montreal_Waterfront-Montreal_Quebec.html"
+    )
+    Recommendation.create!(
+      name: " 3 Hour Montreal City Bike Tour with Wine or Beer",
+      description: "Explore Montreal by bike to discover the city’s colorful neighborhoods, historic buildings, and cuisine. You’ll get a great introduction to Montreal bike paths and bike lanes, too; since your bike rental is good for the whole day, you can keep sightseeing on your own after the tour concludes. Choose between a 9-mile (14-km.) City Tour or the 12-mile (20-km.) Architecture Tour to see the city with a local guide.
+    ",
+      category: "biking",
+      price_range: "$",
+      price: 86,
+      address: "1455, Peel street 3rd Floor, Montreal, Quebec H3A 1T5 Canada",
+      longitude: -73.55361088701993,
+      latitude: 45.50828780903704,
+      kid_friendly: false,
+      dog_friendly: false,
+      outdoor: true,
+      image_url: "https://media-cdn.tripadvisor.com/media/attractions-splice-spp-720x480/06/6e/e2/ef.jpg",
+      start_time: "10:00:00",
+      end_time: "15:00:00",
+      web_url:"https://www.tripadvisor.ca/AttractionProductReview-g155032-d11451195-3_Hour_Montreal_City_Bike_Tour_with_Wine_or_Beer_AM_PM-Montreal_Quebec.html"
+      )
+
+
+
+
 puts "Recommendations created successfully!"
 
 # Print final counts for verification
